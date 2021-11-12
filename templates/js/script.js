6f33785a9d66e3b6e6b52fe374ff5560e4da10aa
Eldanizjb <eldanizjb@.code.edu.az>
2021-11-12 09:05:57 +0400

    modified accardion

--- templates/js/script.js
+++ templates/js/script.js
@@ -102,11 +102,17 @@ for (i = 0; i < toggler.length; i++) {
 }
 
 
-function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
