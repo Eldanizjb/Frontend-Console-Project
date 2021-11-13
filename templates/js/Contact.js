be780aa8f32c186294f7b14e88399f7b140d065b
Eldanizjb <eldanizjb@.code.edu.az>
2021-11-13 19:58:25 +0400

    Create seven page and modifaed

---
+++ templates/js/Contact.js
@@ -0,0 +1,28 @@
+// let names = document.querySelector("[name='name']").value;
+// let emails = document.querySelector("[name='email']").value;
+// let phones = document.querySelector("[name='phone_number']").value;
+// let subjects = document.querySelector("[name='msg_subject']").value;
+// let messages = document.querySelector("[name='message']").value;
+
+// if (names == "") {
+//     document.querySelector(".first").style.display = "inline";
+// };
+// if (emails == "") {
+//     document.querySelector(".second").style.display = "inline";
+// };
+// if (phones == "") {
+//     document.querySelector(".third").style.display = "inline";
+// };
+// if (subjects == "") {
+//     document.querySelector(".four").style.display = "inline";
+// };
+// if (messages == "") {
+//     document.querySelector(".five").style.display = "inline";
+// };
+
+// if ()
+
+
+//     if (names == "" || emails == "" || phones == "" || subjects == "" || messages == "") {
+//     return;
+// }
